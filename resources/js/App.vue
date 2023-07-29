7b88101971783b25c4ca8dde43b67bc7a9e82ff5
falya128 <falya128@users.noreply.github.com>
2023-07-30 08:33:06 +0900

    ルートおよびビューの修正

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,17 +1,3 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="ja">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Laravel Fortify + Sanctum</title>
-      @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-
-    <body>
-      <div id="app">
-        <router-view />
-      </div>
-    </body>
-  </html>
+  <router-view />
 </template>
